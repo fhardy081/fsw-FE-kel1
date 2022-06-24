a8f69744571270d8ab9c25fcd1c82f1cece0d28c
robialakbar <robialakbar@gmail.com>
2022-06-24 17:32:23 +0700

    login page dan info product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import RegistrationPage from "./pages/RegistrationPage";
-import Homepage from "./pages/homepage";
+import Homepage from "./pages/Homepage";
 import ProductPage from "./pages/ProductPage";
+import InfoProduct from "./pages/InfoProduct";
+import RegistrationPage from "./pages/RegistrationPage";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,13 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/products" element={<ProductPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/infoproduct" element={<InfoProduct />} />
       </Routes>
-      {/* <RegistrationPage/> */}
     </BrowserRouter>
-
-
-
-
   )
 }
 
