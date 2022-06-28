1ed1f8dd978008e8c0d906e671702bb5f9907a61
robialakbar <robialakbar@gmail.com>
2022-06-28 18:21:10 +0700

    fix bug

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import '../components/css/style.css'
-
-const Login = () => {
-
-    return (
-        <>
-            <div className="row align-items-center">
-                <div className="col-md">
-                    <img src="/assets/img/img_login.png" className="img-fluid" alt="login" />
-                </div>
-                <div className="col-md">
-                    <div className='row container-fluid'>
-                        <div className="form-title col-md-8 offset-md-2 mb-1">Masuk</div>
-                        <form className='col-md-8 offset-md-2 row' action='#'>
-                            <div className="col-md-12 mb-3">
-                                <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" className="form-control" id="email" placeholder="Contoh: johndee@gmail.com" />
-                            </div>
-                            <div className="col-md-12 mb-3">
-                                <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="password" placeholder="Masukkan password" />
-                            </div>
-                            <div className="col-md-12 mb-3 d-grid">
-                                <button type="submit" className="btn btn-primary">Masuk</button>
-                            </div><br />
-                            <p className='text-center'>Belum Punya Akun? <a className='text-primary' href='/register'><b>Daftar di sini</b></a></p>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-
-    )
-}
-
-=======
 import '../components/css/style.css'
 
 const Login = () => {
@@ -72,5 +35,4 @@ const Login = () => {
     )
 }
 
->>>>>>> 11f7d019fa6514f1dbb2c5c5136d42291eaa9fd3
 export default Login
