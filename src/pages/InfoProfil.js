a9370ae799066a5a3d28fad96622fd15f39daa43
Fitri Dwi Rahmatulaini <fitridwirahma11@gmail.com>
2022-07-11 22:12:33 +0700

    info-profil

--- src/pages/InfoProfil.js
+++ src/pages/InfoProfil.js
@@ -1,77 +1,54 @@
-import React from "react";
 import '../components/css/info-profil.css'
 
 const InfoProfil = () => {
     return (
         <div id="info-profil">
             {/*Nav*/}
-            <nav className="navbar navbar-expand-lg sticky-top">
-                <div className="container-fluid px-5">
-                    <a className="navbar-brand" href="/" />
-                    <h2 className="title text-center">Lengkapi Info Akunmu</h2>
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top d-none d-sm-block" style={{ boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.15)" }}>
+                <div className="container">
+                    <a className="navbar-brand me-auto" id="logo" href="/">&nbsp;</a>
+                    <p className="me-auto mb-1">Lengkapi Info Akun</p>
                 </div>
             </nav>
             {/* Foto Profil */}
-                <div className="profpic"></div>
+            {/* <div className="profpic"></div> */}
             {/*Info*/}
-            <div className="container" id="card">
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">
-                        Nama
-                    </label>
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="exampleFormControlInput1"
-                        placeholder="Nama"
-                        required=""
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">
-                        Kota
-                    </label>
-                    <select
-                        className="form-select form-control mb-3"
-                        aria-label=".form-select example"
-                        required=""
-                    >
-                        <option selected="">Pilih Kota</option>
-                        <option value={1}>Jakarta</option>
-                        <option value={2}>Bandung</option>
-                        <option value={3}>Semarang</option>
-                        <option value={4}>Surabaya</option>
-                        <option value={5}>Bali</option>
-                    </select>
-                </div>
-                
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">
-                        Alamat
-                    </label>
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="exampleFormControlInput1"
-                        placeholder="Contoh: Jalan Ikan Hiu 33"
-                        required=""
-                    />
-                    {/* <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea> */}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">
-                        No.Handphone
-                    </label>
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="exampleFormControlInput1"
-                        placeholder="contoh: +628123456789"
-                        required=""
-                    />
-                </div>
-                <div className="mb-3">
-                    <button className="btn-submit">Submit</button>
+            <div className='container'>
+                <div className='row satu'>
+                    <div className='col-md-3' style={{ marginBottom: '2rem' }}>
+                        <a href=''><i className="bi bi-arrow-left offset-md-5" style={{ fontSize: '1.5rem', color: 'black' }}></i></a>
+                        <span className='title'><center style={{ marginTop: '-1.875rem' }}>Lengkapi Info Akun</center></span>
+                    </div>
+                    <form action='#' className='col-md-6'>
+                        <div className="col-md mb-3">
+                            <center><label><img src='/assets/img/Group_2.png' alt='' /><input id="foto_profil" type={'file'} accept=".jpg,.png" hidden /></label></center>
+                        </div>
+                        <div className="col-md mb-3">
+                            <label htmlFor="nm_produk" className="form-label">Nama<span style={{ color: "red" }}>*</span></label>
+                            <input type="type" className="form-control" id="nama" placeholder="Nama" />
+                        </div>
+                        <div className="col-md mb-3">
+                            <label htmlFor="kategori" className="form-label">Kota<span style={{ color: "red" }}>*</span></label>
+                            <select class="form-control" id="kota">
+                                <option>Pilih Kota</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                            </select>
+                        </div>
+                        <div className="col-md mb-3">
+                            <label htmlFor="deskripsi" className="form-label">Alamat<span style={{ color: "red" }}>*</span></label>
+                            <textarea class="form-control" id="alamat" rows="3" placeholder='Contoh: Jalan Ikan Hiu 33'></textarea>
+                        </div>
+                        <div className="col-md mb-3">
+                            <label htmlFor="nm_produk" className="form-label">No Handphone<span style={{ color: "red" }}>*</span></label>
+                            <input type="type" className="form-control" id="no_hp" placeholder="contoh: +628123456789" />
+                        </div>
+                        <div className="mb-5 d-grid">
+                            <button type="submit" className="btn btn-primary">Simpan</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
