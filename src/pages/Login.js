c326ef80fcedd4ce10c92036684cb3b0e32b3161
Fitri Dwi Rahmatulaini <fitridwirahma11@gmail.com>
2022-07-14 15:50:02 +0700

    fetch login dan reegister

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,58 @@
 import '../components/css/style.css'
+import { Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useRef, useState } from "react";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const emailField = useRef("");
+    const passwordField = useRef("");
+
+    const [errorResponse, setErrorResponse] = useState({
+        isError: false,
+        message: "",
+    });
+
+    const onLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const userToLoginPayload = {
+                email: emailField.current.value,
+                password: passwordField.current.value,
+            };
+
+            const loginRequest = await axios.post(
+                "https://secondhand-kel1.herokuapp.com/api/v1/login",
+                userToLoginPayload
+            );
+            
+            const loginResponse = loginRequest.data;
+
+            if (loginRequest.status === 200) {
+                localStorage.setItem("token", loginResponse.access_token);
+
+                navigate("/");
+            }
+        } catch (err) {
+            if (err.response) {
+                console.log(err);
+                const response = err.response.data;
+
+                setErrorResponse({
+                    isError: true,
+                    message: response.message,
+                });
+            }else{
+                setErrorResponse({
+                    isError: true,
+                    message: err.message
+                })
+            }
+        }
+    };
 
     return (
         <>
@@ -11,16 +63,19 @@ const Login = () => {
                     </div>
                     <div className="col-md">
                         <div className='row container-fluid'>
+                            {errorResponse.isError && (
+                                <Alert variant="danger">{errorResponse.message}</Alert>
+                            )}
                             <a href=''><i className="bi bi-arrow-left arrow-login" style={{ fontSize: '1.5rem', color: 'black', marginBottom: '2rem' }}></i></a>
                             <h2 className='col-md-8 offset-md-2 mrl'><b>Masuk</b></h2>
-                            <form className='col-md-8 offset-md-2 row' action='#'>
+                            <form className='col-md-8 offset-md-2 row' onSubmit={onLogin}>
                                 <div className="col-md-12 mb-3">
                                     <label htmlFor="email" className="form-label">Email</label>
-                                    <input type="email" className="form-control input-width-login" id="email" placeholder="Contoh: johndee@gmail.com" />
+                                    <input type="email" className="form-control input-width-login" ref={emailField} placeholder="Contoh: johndee@gmail.com" />
                                 </div>
                                 <div className="col-md-12 mb-3">
                                     <label htmlFor="password" className="form-label">Password</label>
-                                    <input type="password" className="form-control input-width-login" id="password" placeholder="Masukkan password" />
+                                    <input type="password" className="form-control input-width-login" ref={passwordField} placeholder="Masukkan password" />
                                 </div>
                                 <div className="col-md-12 mb-3 d-grid">
                                     <button type="submit" className="btn btn-primary input-width-login">Masuk</button>
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,48 +1,96 @@
-import React from 'react';
+import { Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useRef, useState } from "react";
 
 const RegistrationPage = () => {
+    const navigate = useNavigate();
+
+    const nameField = useRef("");
+    const emailField = useRef("");
+    const passwordField = useRef("");
+
+    const [errorResponse, setErrorResponse] = useState({
+        isError: false,
+        message: "",
+    });
+
+    const onRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            const userToRegisterPayload = {
+                name: nameField.current.value,
+                email: emailField.current.value,
+                password: passwordField.current.value,
+            };
+
+            const registerRequest = await axios.post(
+                "https://secondhand-kel1.herokuapp.com/api/v1/register",
+                userToRegisterPayload
+            );
+
+            const registerResponse = registerRequest.data;
+
+            if (registerResponse.status) {
+                navigate("/login")
+            }
+        } catch (err) {
+            console.log(err);
+            const response = err.response.data;
+
+            setErrorResponse({
+                isError: true,
+                message: response.message,
+            });
+        }
+    };
+
     return (
         <>
-        <div className="row" style={{marginRight: 0}}>
-            <div className="col-md-6">
-                <div className="image-registration">
-                    <img src="assets/images/register.png" alt="Second Hand" width="100%" height="100%"></img>
+            <div className="row" style={{ marginRight: 0 }}>
+                <div className="col-md-6">
+                    <div className="image-registration">
+                        <img src="assets/images/register.png" alt="Second Hand" width="100%" height="100%"></img>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center">
+                    <form className="form" onSubmit={onRegister}>
+                        <div className="form-body">
+                            {errorResponse.isError && (
+                                <Alert variant="danger">{errorResponse.message}</Alert>
+                            )}
+                            <div className="back-button-register">
+                                <a href="/"><img src="assets/images/fi_arrow-left.png" alt="Button Back"></img>&nbsp;</a>
+                            </div>
+                            <div className="form-title">
+                                Daftar
+                            </div>
+                            <div className="Nama">
+                                <label className="form__label" htmlFor="Name">Nama </label>
+                                <input className="form__input" type="text" ref={nameField} placeholder="Nama Lengkap" />
+                            </div>
+                            <div className="email">
+                                <label className="form__label" htmlFor="email">Email </label>
+                                <input type="email" ref={emailField} className="form__input" placeholder="Contoh: johndee@gmail.com" />
+                            </div>
+                            <div className="password">
+                                <label className="form__label" htmlFor="password">Password </label>
+                                <input className="form__input" type="password" ref={passwordField} placeholder="Masukkan Password" />
+                            </div>
+                        </div>
+                        <div className="footer">
+                            <button type="submit" className="btn-register" style={{ justifyContent: 'center', alignItems: 'center' }}>Daftar</button>
+                            <div className="login-direct">
+                                Sudah punya akun? &nbsp;
+                                <a href='/login' style={{ color: '#7126B5', fontWeight: 'bold' }}>Masuk di sini</a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <div className="col-md-6 d-flex justify-content-center">
-                <div className="form">
-                    <div className="form-body">
-                        <div className="back-button-register">
-                            <a href="/"><img src="assets/images/fi_arrow-left.png" alt="Button Back"></img>&nbsp;</a>
-                        </div>
-                        <div className="form-title">
-                            Daftar
-                        </div>
-                        <div className="Nama">
-                            <label className="form__label" for="Name">Nama </label>
-                            <input className="form__input" type="text" id="Name" placeholder="Nama Lengkap"/>
-                        </div>
-                        <div className="email">
-                            <label className="form__label" for="email">Email </label>
-                            <input  type="email" id="email" className="form__input" placeholder="Contoh: johndee@gmail.com"/>
-                        </div>
-                        <div className="password">
-                            <label className="form__label" for="password">Password </label>
-                            <input className="form__input" type="password"  id="password" placeholder="Masukkan Password"/>
-                        </div>
-                    </div>
-                    <div className="footer">
-                        <button type="submit" className="btn-register" style={{justifyContent: 'center', alignItems: 'center'}}>Daftar</button>
-                        <div className="login-direct">
-                            Sudah punya akun? &nbsp;
-                            <a href='/login' style={{color: '#7126B5', fontWeight: 'bold'}}>Masuk di sini</a>
-                        </div>
-                    </div>
-                </div> 
-            </div>
-        </div>
         </>
-           
-    )    
+
+    )
 }
 export default RegistrationPage;
