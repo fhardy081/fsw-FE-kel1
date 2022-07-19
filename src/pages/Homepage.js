90d00dfa78d5d669187cacdc4e18445abbe34a94
Fitri Dwi Rahmatulaini <fitridwirahma11@gmail.com>
2022-07-19 22:06:29 +0700

    newproduct

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -55,9 +55,9 @@ const Homepage = () => {
             <span class="navbar-toggler-icon"></span>
           </button>
           {/* <a class="navbar-brand" href="#">Navbar</a> */}
-          <form class="navbar-brand">
+          <form>
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
             </form>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
--- src/pages/InfoProduct.js
+++ src/pages/InfoProduct.js
@@ -7,52 +7,77 @@ import '../components/css/style.css'
 
 const InfoProduct = () => {
     const navigate = useNavigate();
-    const [setData] = useState([]);
+    const [data, setData] = useState({});
+    const [photos,setPhotos] = useState([])
     const nameField = useRef("");
     const priceField = useRef("");
     const categoryField = useRef("");
     const descriptionField = useRef("");
-    const [photoField, setphotoField] = useState([]);
     const fileInputRef = useRef();
-    const [is_sold] = useState(Boolean);
 
     const [errorResponse, setErrorResponse] = useState({
         isError: false,
         message: "",
     });
 
+    const setphotoField = async (file) => {
+        try{
+            const form = new FormData();
+            form.append("file",file)
+
+            const image = await api.post("/api/v1/uploadphotoproduct", form,{
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            })
+            const oldPhotos = [...photos,image.data.path]
+            setPhotos(oldPhotos)
+        }catch (e){
+
+        }
+    }
+
     const onPost = async (e) => {
         e.preventDefault();
-
+        console.log(photos)
         try {
-            const postPayload = new FormData();
-            postPayload.append("name", nameField.current.value);
-            postPayload.append("price", priceField.current.value);
-            postPayload.append("category_id", categoryField.current.value);
-            postPayload.append("description", descriptionField.current.value);
-            postPayload.append("photo", photoField);
-            postPayload.append("is_sold", is_sold);
+
+            const postPayload = {
+                name: nameField.current.value,
+                price: priceField.current.value,
+                category: parseInt(categoryField.current.value),
+                description: descriptionField.current.value,
+                photo: photos,
+            }
 
             const postRequest = await api.post(
                 "/api/v1/newproduct", postPayload);
 
-            const postResponse = postRequest.data;
-
-            if (postRequest.status) {
+            if (postRequest.status === 201) {
                 setData(postRequest.data.newProduct);
-                // const endpointid = postResponse.data.newProduct.user_id;
-
-                navigate(`/listproduct`)
+                navigate(`/`)
+            }else{
+                setErrorResponse({
+                    isError: true,
+                    message: postRequest.data.message
+                })
             }
 
         } catch (err) {
-            console.log(err);
-            const response = err.response.data;
+            if (err.response) {
+                console.log(err);
+                const response = err.response.data;
 
-            setErrorResponse({
-                isError: true,
-                message: response.message,
-            });
+                setErrorResponse({
+                    isError: true,
+                    message: response.message,
+                });
+            }else{
+                setErrorResponse({
+                    isError: true,
+                    message: err.message
+                })
+            }
         }
     };
 
@@ -72,7 +97,7 @@ const InfoProduct = () => {
                         <a href=''><i className="bi bi-arrow-left offset-md-5" style={{ fontSize: '1.5rem', color: 'black' }}></i></a>
                         <span className='title'><center style={{ marginTop: '-1.875rem' }}>Lengkapi Detail Product</center></span>
                     </div>
-                    <form action='#' className='col-md-6' enctype="multipart/form-data">
+                    <form action='' onSubmit={onPost} className='col-md-6' enctype="multipart/form-data">
                         <div className="col-md mb-3">
                             <label htmlFor="nm_produk" className="form-label">Nama Produk</label>
                             <input type="type" className="form-control" ref={nameField} placeholder="Nama Produk" />
@@ -84,12 +109,12 @@ const InfoProduct = () => {
                         <div className="col-md mb-3">
                             <label htmlFor="kategori" className="form-label">Kategori</label>
                             <select class="form-control" ref={categoryField}>
-                                <option>Pilih Kategori</option>
-                                <option value="Hobi">Hobi</option>
-                                <option value="Kendaraan">Kendaraan</option>
-                                <option value="Baju">Baju</option>
-                                <option value="Elektronik">Elektronik</option>
-                                <option value="Kesehatan">Kesehatan</option>
+                                <option value="0">Pilih Kategori</option>
+                                <option value="1">Hobi</option>
+                                <option value="2">Kendaraan</option>
+                                <option value="3">Baju</option>
+                                <option value="4">Elektronik</option>
+                                <option value="5">Kesehatan</option>
                             </select>
                         </div>
                         <div className="col-md mb-3">
@@ -99,10 +124,8 @@ const InfoProduct = () => {
                         <div className="col-md mb-3">
                             <div className='row'>
                                 <div className='col-md-3'>
-                                    <label htmlFor="foto" className="form-label" onClick={(e) => {
-                                        e.preventDefault(); fileInputRef.current.click();
-                                    }}>Foto Produk<br /><img src='/assets/img/Group_1.png' alt='' />
-                                        <input type={'file'} accept=".jpg,.png" ref={fileInputRef}
+                                    <label htmlFor="foto" className="form-label">Foto Produk<br /><img src='/assets/img/Group_1.png' alt='' />
+                                        <input type={'file'} id="foto" accept=".jpg,.png" ref={fileInputRef}
                                             onChange={(e) => {
                                                 setphotoField(e.target.files[0])
                                             }} hidden />
@@ -115,7 +138,7 @@ const InfoProduct = () => {
                         </div>
                         <div className='row'>
                             <div className="col-6 mb-3 d-grid">
-                                <button className="btn btn-outline-primary" onClick={(e) => onPost(e, false)} type="submit">Preview</button>
+                                <button className="btn btn-outline-primary" type="submit">Preview</button>
                             </div>
                             <div className="col-6 mb-3 d-grid">
                                 <button type="submit" className="btn btn-primary" onClick={(e) => onPost(e, true)}>Terbitkan</button>
